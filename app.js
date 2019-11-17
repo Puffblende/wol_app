6fc48d8065410ef967652cb9706df605b9e0f6da
Nessim Saidi <nessim.saidi@googlemail.com>
2019-11-17 18:43:37 +0100

    FIX: express request paramater read from query

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/wol/wakeup', (req, res) => {
-  if (req.params("token") === "meinpasswort123") {
+  if (req.query.token === "meinpasswort123") {
     wol.wake('AC:22:0B:75:F4:42',{
     address: '192.168.0.100',
     port: 7
