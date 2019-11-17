d9c714f66415619ca511bf7d074198ee6224949d
Puffblende <dennis.kiefer.business@googlemail.com>
2019-11-17 17:55:05 +0100

    runnable example (hard coded MAC)

--- app.js
+++ app.js
@@ -10,7 +10,16 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/wol/wakeup', (req, res) => {
-  wol.wake('AC:22:0B:75:F4:42');
+  wol.wake('AC:22:0B:75:F4:42',{
+  address: '192.168.0.100',
+  port: 7
+}, function(error) {
+  if(error) {
+    console.log("fuu");
+    return;
+  }
+});
+
   console.log("command recieved");
   res.send('Hello world\n');
 });
