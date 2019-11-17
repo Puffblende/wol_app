be972c43dda22baff8487162801aec3c7cd215cf
Nessim Saidi <nessim.saidi@googlemail.com>
2019-11-17 18:10:34 +0100

    Password test as URL parameter

--- app.js
+++ app.js
@@ -10,18 +10,21 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/wol/wakeup', (req, res) => {
-  wol.wake('AC:22:0B:75:F4:42',{
-  address: '192.168.0.100',
-  port: 7
-}, function(error) {
-  if(error) {
-    console.log("fuu");
-    return;
-  }
-});
+  if (req.params("token") === "meinpasswort123") {
+    wol.wake('AC:22:0B:75:F4:42',{
+    address: '192.168.0.100',
+    port: 7
+  }, function(error) {
+    if(error) {
+      console.log("fuu");
+      return;
+    }
+  });
 
-  console.log("command recieved");
-  res.send('Hello world\n');
+    console.log("command recieved");
+    res.send('Hello world\n');
+  }
+
 });
 
 app.listen(PORT, HOST);
