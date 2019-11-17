0d09e830d657488c97b48e498018c60951cc7a22
Puffblende <dennis.kiefer.business@googlemail.com>
2019-11-17 16:24:43 +0100

    Port change

--- app.js
+++ app.js
@@ -3,12 +3,12 @@
 const express = require('express');
 
 // Constants
-const PORT = 8080;
+const PORT = 8082;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
+app.get('/wol/wakeup', (req, res) => {
   res.send('Hello world\n');
 });
 
