cfb5ce78d63934095f21f9293bb5600f6be9afe6
Puffblende <dennis.kiefer.business@googlemail.com>
2019-11-17 17:28:22 +0100

    insert MAC Address

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const wol = require('node-wol');
 
 // Constants
 const PORT = 8082;
@@ -9,6 +10,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/wol/wakeup', (req, res) => {
+  wol.wake('AC:22:0B:75:F4:42');
   console.log("command recieved");
   res.send('Hello world\n');
 });
